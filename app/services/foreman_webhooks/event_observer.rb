667565d669ca16a3dbe0a34014016a6a495188b0
Kamil Szubrycht <kamil.szubrycht@ironin.pl>
2019-12-18 13:53:20 +0100

    apply plugin api changes

--- app/services/foreman_webhooks/event_observer.rb
+++
@@ -1,7 +0,0 @@
-module ForemanWebhooks
-  class EventObserver
-    def notify(event_name:, payload:)
-      WebhookTarget.deliver(event_name: event_name, payload: payload.to_json)
-    end
-  end
-end
