62d7d603012fab5b41a2c68f39e6a1505b1730e3
Oleh Fedorenko <ofedoren@redhat.com>
2020-11-04 16:44:16 +0000

    Fixes #31261 - Add basic React infrastructure
    Add experimental route
    
    (cherry picked from commit a9f73b47f25e2891f7976b058c80e9cee55d1b17)

---
+++ webpack/routes_index.js
@@ -0,0 +1,4 @@
+import { registerRoutes } from 'foremanReact/routes/RoutingService';
+import ForemanWebhooksRoutes from './ForemanWebhooks/routes';
+
+registerRoutes('foreman_webhooks', ForemanWebhooksRoutes);
