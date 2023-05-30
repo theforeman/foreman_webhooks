bf1294bfbc61bd04f636eb02dd687c38cd3e67c3
Oleh Fedorenko <ofedoren@redhat.com>
2023-05-30 15:07:20 +0000

    Fixes #36454 - Set translations up

---
+++ app/assets/javascripts/foreman_webhooks/locale/en/foreman_webhooks.js
@@ -0,0 +1,238 @@
+ locales['foreman_webhooks'] = locales['foreman_webhooks'] || {}; locales['foreman_webhooks']['en'] = {
+  "domain": "foreman_webhooks",
+  "locale_data": {
+    "foreman_webhooks": {
+      "": {
+        "Project-Id-Version": "foreman_webhooks 3.1.0",
+        "Report-Msgid-Bugs-To": "",
+        "PO-Revision-Date": "2023-05-30 14:58+0000",
+        "Last-Translator": "FULL NAME <EMAIL@ADDRESS>",
+        "Language-Team": "English",
+        "Language": "en",
+        "MIME-Version": "1.0",
+        "Content-Type": "text/plain; charset=UTF-8",
+        "Content-Transfer-Encoding": "8bit",
+        "Plural-Forms": "nplurals=; plural=;",
+        "lang": "en",
+        "domain": "foreman_webhooks",
+        "plural_forms": "nplurals=; plural=;"
+      },
+      "Actions": [
+        ""
+      ],
+      "Additional": [
+        ""
+      ],
+      "Authentication credentials": [
+        ""
+      ],
+      "Authorize with Foreman client certificate and validate smart-proxy CA from Settings": [
+        ""
+      ],
+      "Change the password": [
+        ""
+      ],
+      "Clone a template": [
+        ""
+      ],
+      "Confirm Webhook Deletion": [
+        ""
+      ],
+      "Create Template": [
+        ""
+      ],
+      "Create Webhook": [
+        ""
+      ],
+      "Create a Webhook": [
+        ""
+      ],
+      "Create a webhook template": [
+        ""
+      ],
+      "Credentials": [
+        ""
+      ],
+      "Delete": [
+        ""
+      ],
+      "Delete a Webhook": [
+        ""
+      ],
+      "Delete a webhook template": [
+        ""
+      ],
+      "Deliver webhook": [
+        ""
+      ],
+      "Deliver webhook %s": [
+        ""
+      ],
+      "Edit": [
+        ""
+      ],
+      "Edit %s": [
+        ""
+      ],
+      "Enabled": [
+        ""
+      ],
+      "Error": [
+        ""
+      ],
+      "Example: %s": [
+        ""
+      ],
+      "Export a webhook template to ERB": [
+        ""
+      ],
+      "General": [
+        ""
+      ],
+      "HTTP Content Type": [
+        ""
+      ],
+      "HTTP Headers": [
+        ""
+      ],
+      "HTTP Method": [
+        ""
+      ],
+      "If unchecked, the webhook will be inactive": [
+        ""
+      ],
+      "Import a webhook template": [
+        ""
+      ],
+      "List Webhooks": [
+        ""
+      ],
+      "List available events for subscription": [
+        ""
+      ],
+      "List webhook templates": [
+        ""
+      ],
+      "Name": [
+        ""
+      ],
+      "No Results": [
+        ""
+      ],
+      "Optional CAs in PEM format concatenated to verify the receiver's SSL certificate": [
+        ""
+      ],
+      "Optional. Must be a JSON object (ERB allowed)": [
+        ""
+      ],
+      "Password": [
+        ""
+      ],
+      "Proxy Authorization": [
+        ""
+      ],
+      "Show Webhook details": [
+        ""
+      ],
+      "Show webhook template details": [
+        ""
+      ],
+      "Snippet": [
+        ""
+      ],
+      "Start typing to search": [
+        ""
+      ],
+      "Subscribe to": [
+        ""
+      ],
+      "Target URL": [
+        ""
+      ],
+      "Target URL that should be called by Foreman (ERB allowed).": [
+        ""
+      ],
+      "Template": [
+        ""
+      ],
+      "Template contents including metadata": [
+        ""
+      ],
+      "Template name": [
+        ""
+      ],
+      "This template is locked for editing.": [
+        ""
+      ],
+      "Try to create a new webhook": [
+        ""
+      ],
+      "Type": [
+        ""
+      ],
+      "URL must be valid and schema must be one of: %s": [
+        ""
+      ],
+      "Uncheck this option to disable validation of the receiver's SSL certificate": [
+        ""
+      ],
+      "Update a Webhook": [
+        ""
+      ],
+      "Update a webhook template": [
+        ""
+      ],
+      "User": [
+        ""
+      ],
+      "Verify SSL": [
+        ""
+      ],
+      "Webhook": [
+        ""
+      ],
+      "Webhook %s was successfully deleted": [
+        ""
+      ],
+      "Webhook Templates": [
+        ""
+      ],
+      "Webhook was successfully created.": [
+        ""
+      ],
+      "Webhook was successfully updated.": [
+        ""
+      ],
+      "WebhookTemplate|Locked": [
+        ""
+      ],
+      "WebhookTemplate|Name": [
+        ""
+      ],
+      "WebhookTemplate|Snippet": [
+        ""
+      ],
+      "Webhooks": [
+        ""
+      ],
+      "Whether or not the template is added automatically to new organizations and locations": [
+        ""
+      ],
+      "Whether or not the template is locked for editing": [
+        ""
+      ],
+      "X509 Certification Authorities": [
+        ""
+      ],
+      "X509 Certification Authorities concatenated in PEM format": [
+        ""
+      ],
+      "You are about to delete %s. Are you sure?": [
+        ""
+      ],
+      "is required": [
+        ""
+      ]
+    }
+  }
+};
