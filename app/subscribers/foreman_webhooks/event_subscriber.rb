8e08120efb60460fecb9708899eb318940f740dd
alazik <alazik@redhat.com>
2024-07-19 14:57:50 +0000

    Fixes #37667 - Provide meaningful error message on org/loc mismatch
    Added meaningful error message when webhook template fails to render due
    to organization/location mismatch.
    
    (cherry picked from commit 206702b18ed73e47dae5d578d0e93a38d9e17ac6)

--- app/subscribers/foreman_webhooks/event_subscriber.rb
+++ app/subscribers/foreman_webhooks/event_subscriber.rb
@@ -4,6 +4,8 @@ module ForemanWebhooks
   class EventSubscriber < ::Foreman::BaseSubscriber
     def call(event)
       ::Webhook.deliver(event_name: event.name, payload: event.payload)
+    rescue ::Foreman::Exception => e
+      Rails.logger.error e.message
     end
   end
 end
