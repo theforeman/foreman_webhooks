f3ba55764c32041313cda52baff3e1b107a05ac1
Oleh Fedorenko <ofedoren@redhat.com>
2025-01-30 14:55:51 +0000

    Fixes #38176 - Make payload signature less strict

--- app/lib/foreman_webhooks/renderer/scope/webhook_template.rb
+++ app/lib/foreman_webhooks/renderer/scope/webhook_template.rb
@@ -21,14 +21,17 @@ module ForemanWebhooks
         end
 
         apipie :method, 'Creates final payload' do
-          required :hash, Hash, 'Key=value object with with data that should be present in payload'
+          optional :hash, Hash, 'Key:value object with with data that should be present in payload', default: {}
           keyword :with_defaults, [true, false], 'If set to true, adds default entries to the payload', default: true
+          kwlist :kwargs, 'Additional key:value pairs that should be added to the payload'
           returns String, 'JSON string with the final payload'
           example 'payload({ id: @object.id, name: @object.name }) #=> ' \
                   '"{ "id": 1, "name": "host.example.com", "context": { ... }, ' \
                   '"event_name": "host_created.event.foreman" }"'
         end
-        def payload(hash, with_defaults: true)
+        def payload(hash = {}, with_defaults: true, **kwargs)
+          raise ArgumentError, 'The first argument must be either a hash or a comma-separated list with key:value pairs' unless hash.is_a?(Hash)
+          hash.merge!(kwargs)
           hash.merge!(@defaults) if with_defaults
           hash.to_json
         end
