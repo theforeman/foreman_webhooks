8c9c725ff67c244926630d9595c67e3f0b9cb9ee
Oleh Fedorenko <ofedoren@redhat.com>
2025-08-06 15:14:38 +0000

    Fix rubocop errors

--- app/models/webhook.rb
+++ app/models/webhook.rb
@@ -131,6 +131,7 @@ class Webhook < ApplicationRecord
 
   def rendered_payload(event_name, payload)
     raise ::Foreman::Exception.new(N_("Webhook template not found while firing webhook '%s'. Possible organization/location mismatch in the webhook template?"), name) if webhook_template.nil?
+
     webhook_template.render(variables: variables(event_name, payload))
   end
 
